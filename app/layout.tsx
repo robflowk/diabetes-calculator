c977b346e2478bacd168a0a80f5c2d5d4ede8f37
Robin Wloka <robin.wloka@red-oak-consulting.com>
2024-01-09 09:35:32 +0100

    Apple Icon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Diabetes Calculator',
-  description: 'Berechnen von Brot- und Kohlenhydrateinheiten für Diabetiker.',
-}
+  title: "Diabetes Calculator",
+  description: "Berechnen von Brot- und Kohlenhydrateinheiten für Diabetiker.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/diacalc.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
